04439dd30abf69bf665209636b6c5c07a1593a2c
guobk <836571084@qq.com>
2021-03-04 22:24:57 +0800

    引入elementUI

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 // 设置反向代理，前端请求默认发送到 http://localhost:8443/panda
 var axios = require('axios')
 axios.defaults.baseURL = 'http://localhost:8443/panda'
@@ -10,9 +13,12 @@ axios.defaults.baseURL = 'http://localhost:8443/panda'
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  render: h => h(App),
   router,
   components: { App },
   template: '<App/>'
